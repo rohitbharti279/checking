198b363e2f4af32cfb35b2effacec30d436e0478
Rohit Bharti <rohitbharti279@gmail.com>
2024-01-18 14:48:46 +0530

    practice

--- main.js
+++ main.js
@@ -1,43 +1,20 @@
-function threeSum(nums) {
-    const result = [];
-  
-    nums.sort((a, b) => a - b);
-  
-    for (let i = 0; i < nums.length - 2; i++) {
-      if (i > 0 && nums[i] === nums[i - 1]) {
-        continue;
-      }
-  
-      let left = i + 1;
-      let right = nums.length - 1;
-  
-      while (left < right) {
-        const sum = nums[i] + nums[left] + nums[right];
-  
-        if (sum === 0) {
-          result.push([nums[i], nums[left], nums[right]]);
-  
-          while (left < right && nums[left] === nums[left + 1]) {
-            left++;
-          }
-  
-          while (left < right && nums[right] === nums[right - 1]) {
-            right--;
-          }
-  
-          left++;
-          right--;
-        } else if (sum < 0) {
-          left++;
-        } else {
-          right--;
-        }
-      }
-    }
-  
-    return result;
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+  const obj = {
+      2: "abc",
+      3: "def",
+      4: "ghi",
+      5: "jkl",
+      6: "mno",
+      7: "pqrs",
+      8: "tuv",
+      9: "wxyz",
   }
+  console.log(obj);
+};
+// const phone_map = ["abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"];
 
-console.log(threeSum([-1, 0, 1, 2, -1, -4]));
-console.log(threeSum([0,1,1]));
-console.log(threeSum([0,0,0]));
+console.log(letterCombinations("23"))
