f908a1d093ea5ea09b35c458bef9dbd0534a4812
Rohit Bharti <rohitbharti279@gmail.com>
2024-01-30 11:55:43 +0530

    update

---
+++ api/data.js
@@ -0,0 +1,6 @@
+const data = [
+  { firstName: "Rohit", lastName: "Bharti", emal: "rohit@gmail.com", contact: 8804073066 },
+  { firstName: "ankit", lastName: "choraria", emal: "ankit@gmail.com", contact: 1234567899 },
+  { firstName: "sam", lastName: "Bahadur", emal: "sam@gmail.com", contact: 1234567899 },
+];
+module.exports = data;
